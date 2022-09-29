1b535d2e6c03f2ccd11c933cdb1fdbd144b535e4
Leonard Makafui Senahey <leonardsenahey@gmail.com>
2022-09-29 09:30:02 +0000

    Domain and Hosting

---
+++ application/config/database.php
@@ -0,0 +1,33 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+/*
+ *
+ * Edit this file and upate with your own MySQL database details.
+*/
+
+
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'hosting';
+
+
+
+
+/** Do not change the below values. Change only if you know what you are doing */
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = 'hd_';
+$db['default']['pconnect'] = FALSE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = "utf8";
+$db['default']['dbcollat'] = "utf8_unicode_ci";
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
+$active_group = 'default';
+$query_builder = TRUE;
+/* End of file database.php */
+/* Location: ./application/config/database.php */
